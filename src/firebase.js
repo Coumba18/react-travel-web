430b4e514c928438e888501b74541be449714cdc
Coumba Ngom <kumbugaan@gmail.com>
2022-10-07 16:07:42 +0000

    firebase.js ajouté

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAOvKC-DXwEI9LIWYwTutoELga28t7ghu4",
+  authDomain: "travel-web-edcee.firebaseapp.com",
+  projectId: "travel-web-edcee",
+  storageBucket: "travel-web-edcee.appspot.com",
+  messagingSenderId: "235269414063",
+  appId: "1:235269414063:web:b7811af1b6cd3eb709485a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
